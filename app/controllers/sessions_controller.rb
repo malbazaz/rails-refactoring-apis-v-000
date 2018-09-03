b82e2e9be47a58342e4ab40de072452fe2ad210e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-03 16:05:10 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
-    
+    github_repo = GithubRepo.new
+    session[:token]
     redirect_to '/'
   end
 end
